928e0998baa9791d3b805658760308ea2416d7b9
benyaminghn <benjaminghahremani@gmail.com>
2021-09-14 20:24:56 +0430

    first commit of the dataset preparation

--- dataset/dataset_base.py
+++ dataset/dataset_base.py
@@ -1,8 +1,10 @@
-
+from glob import glob # for listing the directory of dataset
+import skimage.io as io # to read the .mhd and .raw data
+import os
 
 class DatasetBase:
 
-    def __init__(self, batch_size, input_res):
+    def __init__(self, batch_size, input_res, ):
 
         """
         Handles data ingestion: preparing, pre-processing, augmentation, data generators
@@ -14,7 +16,7 @@ class DatasetBase:
         self.batch_size = batch_size
         self.input_res = input_res
 
-    def create_data_generators(self, dataset_dir):
+    def create_data_generators(self, dataset_dir, ):
 
         """Creates data generators based on batch_size, input_res
 
@@ -26,5 +28,15 @@ class DatasetBase:
         :returns n_iter_val: number of iterations per epoch for val_data_gen
 
         """
+    def fetch_data(self, dataset_dir="D:/AIMedic/FinalProject_echocardiogram/echoC_Dataset/CAMUS/training/"):
 
-        raise Exception('not implemented!')
+        """
+        fetching data from directory of A4C view images of CAMUS dataset
+
+        :param dataset_dir: directory address of the dataset
+
+        :return: data_dir_list: list of the directory of each A4C view image
+        :return: labels: list of the directory of each type map labels
+        """
+        X_4CH_ED_dir = glob(os.path.join(dataset_dir,'/*/*_4CH_ED.mhd'))
+        y_4CH_ED_dir = glob(os.path.join(dataset_dir,'/*/*_4CH_ED_gt.mhd'))
