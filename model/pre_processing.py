64efc75f6a008259dd34d3c0c4885221b1cd3465
soroush <soroush.moazed@gmail.com>
2021-09-23 08:41:10 +0330

    syntax

--- model/pre_processing.py
+++ model/pre_processing.py
@@ -3,6 +3,7 @@ import numpy as np
 
 
 class PreProcessor:
+
     """
      PreProcess module used for images, batches, generators
 
@@ -14,13 +15,16 @@ class PreProcessor:
     """
 
     def __init__(self, target_size=(128, 128)):
+
         """
         :param target_size: image target size for resizing, tuple (image_height, image_width)
         """
+
         self.target_size = target_size
         self.normalization = True
 
     def img_preprocess(self, image):
+
         """
         pre-processing on input image
 
@@ -28,6 +32,7 @@ class PreProcessor:
 
         :return: pre_processed_img: pre-processed image
         """
+
         # 1. normalization on the given image
         if self.normalization:
             normalized_image = self.rescaling(image, 1/255.)
@@ -38,6 +43,7 @@ class PreProcessor:
         return pre_processed_img
 
     def batch_preprocess(self, batch):
+
         """
         batch pre_processing function
 
@@ -46,6 +52,7 @@ class PreProcessor:
         :return: x_preprocessed_batch: preprocessed batch for x
         :return: y_preprocessed_batch: preprocessed batch for y
         """
+
         # images of the give batch
         x = batch[0]
 
@@ -60,12 +67,14 @@ class PreProcessor:
         return x_preprocessed_batch, y_preprocessed_batch
 
     def add_preprocess(self, generator):
+
         """providing the suggested pre-processing for the given generator
 
         :param generator: input generator ready for pre-processing, data generator < class DataGenerator >
 
         :return: preprocessed_gen: preprocessed generator, data generator < class DataGenerator >
         """
+
         while True:
             batch = next(generator)
             pre_processed_batch = self.batch_preprocess(batch)
@@ -74,6 +83,7 @@ class PreProcessor:
         # return pre_processed_gen
 
     def resizing(self, image):
+
         """
         resizing image into the target_size dimensions
 
@@ -90,6 +100,7 @@ class PreProcessor:
 
     @staticmethod
     def rescaling(image, rescaling_ratio=1/255.):
+
         """
         rescaling the input image
 
@@ -98,6 +109,7 @@ class PreProcessor:
 
         :return: rescaled image
         """
+
         return image * rescaling_ratio
 
     def augmentation(self):
@@ -125,22 +137,29 @@ class PreProcessedGen(tf.keras.utils.Sequence):
         self.generator.on_epoch_end()
 
     def __next__(self):
+
         """create a generator that iterate over the Sequence."""
+
         return self.next()
 
     def next(self):
+
         """
         Create iteration through batches of the generator
         :return: next batch, np.ndarray
         """
+
         index = next(self.generator.flow_index())
         return self.__getitem__(index)
 
     def reset(self):
+
         """reset indexes for iteration"""
+
         self.generator.reset()
 
     def random_visualization(self):
+
         """random visualization of an image"""
 
         # choosing random image from dataset random indexes
