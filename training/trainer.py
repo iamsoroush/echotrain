9d16ae6f65701be2d53d87153e8351f49a4781a0
soroush <soroush.moazed@gmail.com>
2021-10-04 12:08:05 +0330

    add params from config file to mlflow

--- training/trainer.py
+++ training/trainer.py
@@ -1,6 +1,8 @@
 import os
 import re
 from shutil import copy
+from pathlib import Path
+import yaml
 
 from tensorflow import keras
 import mlflow
@@ -9,7 +11,7 @@ from mlflow.tracking import MlflowClient
 
 class Trainer:
 
-    def __init__(self, base_dir, config):
+    def __init__(self, base_dir: Path, config):
 
         """
         handles: MLFlow, paths, callbacks(tensorboard, lr, model checkpointing, ...), continuous training
@@ -40,6 +42,7 @@ class Trainer:
 
         self.base_dir = base_dir
         self._load_params(config)
+        self._check_for_exported()
 
         mlflow.set_tracking_uri(f'file:{self.mlflow_tracking_uri}')
 
@@ -57,7 +60,7 @@ class Trainer:
 
         self.my_callbacks = [
             # keras.callbacks.EarlyStopping(patience=2),
-            keras.callbacks.ModelCheckpoint(filepath=self.checkpoints_addr + checkpoints_name,
+            keras.callbacks.ModelCheckpoint(filepath=self.checkpoints_addr.joinpath(checkpoints_name),
                                             save_weights_only=True,
                                             save_freq=self.callbacks_config.checkpoints.save_freq),
             keras.callbacks.TensorBoard(log_dir=self.tensorboard_log,
@@ -82,10 +85,10 @@ class Trainer:
 
         initial_epoch = 0
         if len(os.listdir(self.checkpoints_addr)):
-            checkpoints = [self.checkpoints_addr + '/' + p for p in os.listdir(self.checkpoints_addr)]
-            latest_checkpoint = max(checkpoints, key=os.path.getctime)
+            # checkpoints = [self.checkpoints_addr.joinpath(p) for p in self.checkpoints_addr.iterdir()]
+            latest_checkpoint = max(self.checkpoints_addr.iterdir(), key=os.path.getctime)
             # initial_epoch = re.findall('model\\.([0-9]+)(-[+-]?[0-9]+\\.[0-9]+)*', latest_checkpoint.split('/')[-1])
-            initial_epoch = int(re.findall('model\\.([0-9]+)', latest_checkpoint.split('/')[-1])[0])
+            initial_epoch = int(re.findall('model\\.([0-9]+)', latest_checkpoint.name)[0])
             model.load_weights(latest_checkpoint)
 
         run_id = self._load_existing_run()
@@ -97,15 +100,20 @@ class Trainer:
         # else:
         #     mlflow.set_experiment(self.mlflow_experiment_name)
         # with mlflow.start_run():
-        with active_run:
+        with active_run as run:
+            # Add params from config file to mlflow
+            self._add_config_file_to_mlflow()
+
             mlflow.tensorflow.autolog(every_n_iter=1,
                                       log_models=False,
                                       disable=False,
                                       exclusive=False,
                                       disable_for_unsupported_versions=False,
                                       silent=False)
+
+            # Write run_id
             with open(self.run_id_path, 'w') as f:
-                f.write(active_run.info.run_id)
+                f.write(run.info.run_id)
             history = model.fit(train_data_gen, steps_per_epoch=n_iter_train, initial_epoch=initial_epoch,
                                 epochs=self.epochs, validation_data=val_data_gen, validation_steps=n_iter_val,
                                 callbacks=self.my_callbacks)
@@ -126,23 +134,30 @@ class Trainer:
         for cp in checkpoints:
             epoch = re.findall('model\\.([0-9]+)', cp)[0]
             metric_values = cp.replace('.hdf5', '').split('-')
-            model_info[epoch] = metric_values[1+metric_number]
+            model_info[epoch] = metric_values[1 + metric_number]
             # metric_values = re.sub('model\\.[0-9]+', '', cp).replace('.hdf5', '')
         if mode == 'min':
             selected_model = min(model_info, key=model_info.get)
         else:
             selected_model = max(model_info, key=model_info.get)
-        selected_checkpoint = checkpoints[int(selected_model)-1]
+
+        selected_checkpoint = checkpoints[int(selected_model) - 1]
         chp_addr = os.path.join(self.checkpoints_addr, selected_checkpoint)
-        dst = os.path.join(self.base_dir, 'exported')
-        if not os.path.isdir(dst):
-            os.makedirs(dst)
-        copy(chp_addr, os.path.join(dst, selected_checkpoint))
+        if not self.exported_dir.is_dir():
+            os.makedirs(self.exported_dir)
+        copy(chp_addr, self.exported_dir.joinpath(selected_checkpoint))
 
-        config_file_name = [i for i in os.listdir(self.base_dir) if i.endswith('.yaml')][0]
-        copy(os.path.join(self.base_dir, config_file_name), os.path.join(dst, 'config.yaml'))
+        copy(self.config_file_path, self.exported_dir.joinpath('config.yaml'))
 
-        return dst
+        return self.exported_dir
+
+    def _check_for_exported(self):
+
+        """Raises exception if exported directory exists and contains some files"""
+
+        if self.exported_dir.is_dir():
+            if any(self.exported_dir.iterdir()):
+                raise Exception('exported files already exist.')
 
     def _load_params(self, config):
 
@@ -152,14 +167,19 @@ class Trainer:
         self.callbacks_config = config.trainer.callbacks
         self.evaluation_metrics = self.callbacks_config.checkpoints.evaluation_metrics
         self.export_config = config.trainer.export
-        self.checkpoints_addr = os.path.join(self.base_dir, 'checkpoints')
-        self.tensorboard_log = os.path.join(self.base_dir, 'logs')
+
+        # Paths
+        self.checkpoints_addr = self.base_dir.joinpath('checkpoints')  # os.path.join(self.base_dir, 'checkpoints')
+        self.tensorboard_log = self.base_dir.joinpath('logs')  # os.path.join(self.base_dir, 'logs')
+        self.run_id_path = self.base_dir.joinpath('run_id.txt')  # os.path.join(self.base_dir, 'run_id.txt')
+        self.exported_dir = self.base_dir.joinpath('exported')
+        config_file_name = [i for i in os.listdir(self.base_dir) if i.endswith('.yaml')][0]
+        self.config_file_path = self.base_dir.joinpath(config_file_name)
 
         # MLFlow
         self.mlflow_tracking_uri = config.trainer.mlflow.tracking_uri
         self.mlflow_experiment_name = config.trainer.mlflow.experiment_name
         self.run_name = config.trainer.mlflow.run_name
-        self.run_id_path = os.path.join(self.base_dir, 'run_id.txt')
 
     def _load_existing_run(self):
 
@@ -167,7 +187,7 @@ class Trainer:
 
         run_id = None
 
-        if os.path.exists(self.run_id_path):
+        if self.run_id_path.exists():
             with open(self.run_id_path, 'r') as f:
                 run_id = f.readline()
 
@@ -200,3 +220,33 @@ class Trainer:
             active_run = mlflow.start_run(experiment_id=experiment_id, run_name=self.run_name)
 
         return active_run
+
+    def _add_config_file_to_mlflow(self):
+
+        """Adds parameters from config file to mlflow"""
+
+        def param_extractor(dictionary):
+
+            """Returns a list of each item formatted like 'trainer.mlflow.tracking_uri: /tracking/uri' """
+
+            values = []
+            for key, value in dictionary.items():
+                if isinstance(value, dict):
+                    items_list = param_extractor(value)
+                    for i in items_list:
+                        values.append(f'{key}.{i}')
+                else:
+                    values.append(f'{key}: {value}')
+            return values
+
+        with open(self.config_file_path) as file:
+            data_map = yaml.safe_load(file)
+
+        str_params = param_extractor(data_map)
+        params = {}
+        for item in str_params:
+            name = item.split(':')[0]
+            item_value = item.split(': ')[-1]
+            params[name] = item_value
+
+        mlflow.log_params(params)
