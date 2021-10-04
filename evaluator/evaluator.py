3e908e8ff3d14791ec5455dc8b0535802bcbea2d
azin <azinjvd@gmail.com>
2021-10-04 14:26:10 +0330

    final evaluator(tested)

--- evaluator/evaluator.py
+++ evaluator/evaluator.py
@@ -1,19 +1,10 @@
+import sys
+sys.path.append('..')
+
 from model import metric
 from model import loss
 import numpy as np
 import pandas as pd
-import os
-import sys
-from model.unet import UNet
-
-import tensorflow as tf
-
-utils_dir = os.path.abspath('../utils')
-sys.path.append(utils_dir)
-from handling_yaml import load_config_file
-
-config_path = "../config/config_example.yaml"
-config = load_config_file(config_path)
 
 
 class Evaluator:
@@ -48,13 +39,14 @@ class Evaluator:
             each_batch = next(data_gen)
             for j in range(len(each_batch[0])):
                 data_featurs = []
+                print(each_batch[1].shape)
                 y_true = each_batch[1][j].reshape((1, self.input_h, self.input_w, self.n_channels))
                 y_pred = model.predict(each_batch[0][j].reshape((1, self.input_h, self.input_w, self.n_channels)))
                 data_featurs.append(float(loss.iou_coef_loss(y_true, y_pred)))
                 data_featurs.append(float(loss.dice_coef_loss(y_true, y_pred)))
                 data_featurs.append(float(loss.soft_dice_loss(y_true, y_pred)))
                 data_featurs.append(float(metric.iou_coef(y_true, y_pred)))
-                #data_featurs.append(float(metric.soft_iou(y_true, y_pred)))
+                # data_featurs.append(float(metric.soft_iou(y_true, y_pred)))
                 data_featurs.append(float(metric.dice_coef(y_true, y_pred)))
                 data_featurs.append(float(metric.soft_dice(y_true, y_pred)))
                 data_featurs.append(self._model_certainty(y_true, y_pred)[0])
@@ -64,10 +56,11 @@ class Evaluator:
                 data_featurs.append(self.true_positive_rate(y_true, y_pred))
                 data_featurs.append(self.true_negative_rate(y_true, y_pred))
                 data_frame_numpy.append(data_featurs)
-        return pd.DataFrame(data_frame_numpy, columns=['iou_coef_loss', 'dice_coef_loss'
-                                                        ,'soft_dice_loss', 'iou_coef', 'dice_coef','soft_dice',
-                                                       'truecertainty', 'falsecertainty','ambigous','certainty_state'
-                                                        ,'true_positive_rate','true_negative_rate'])
+        return pd.DataFrame(data_frame_numpy, columns=['iou_coef_loss', 'dice_coef_loss',
+                                                        'soft_dice_loss', 'iou_coef', 'dice_coef', 'soft_dice',
+                                                       'truecertainty', 'falsecertainty', 'ambigous', 'certainty_state'
+                                                        , 'true_positive_rate', 'true_negative_rate'])
+
     @staticmethod
     def _model_certainty(y_true, y_pred):
         """
@@ -78,7 +71,7 @@ class Evaluator:
         dif = np.abs(y_true- y_pred)
         truecertainty = np.count_nonzero(dif < 0.3)/y_true.size
         falsecertainty = np.count_nonzero(dif > 0.7)/y_true.size
-        ambiguous = np.count_nonzero(np.logical_and(0.3 <= dif,dif <= 0.7))/y_true.size
+        ambiguous = np.count_nonzero(np.logical_and(0.3 <= dif, dif <= 0.7))/y_true.size
         certainty_list = [truecertainty, falsecertainty, ambiguous]
         if np.argmax(certainty_list) == 0:
             return [truecertainty, falsecertainty, ambiguous, 'true_certain']
@@ -114,17 +107,5 @@ class Evaluator:
                 if y_pred[0][i,j] == y_true[0][i,j] and y_true[0][i,j] == 0:
                     TN += 1
         return (TN / np.count_nonzero(y_true > 0.5)) * 100
-
-def data_gen():
-    for i in range(3):
-        x = np.random.randn(25, 256, 256, 1)
-        y = np.random.randn(25, 256, 256, 1)
-        yield x,y
-
-
-unet = UNet(config)
-model = unet.generate_training_model()
-e = Evaluator(config)
-print(e.build_data_frame(model, data_gen(), 3))
 
 
