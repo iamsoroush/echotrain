201ec9478d5d2f8df7cb926fb5d073dc67246b5c
soroush <soroush.moazed@gmail.com>
2021-10-04 13:39:45 +0330

    axis param from dynamic to constant

--- model/metric.py
+++ model/metric.py
@@ -20,7 +20,8 @@ def get_iou_coef(threshold=0.5, smooth=0.001):
         y_pred_thresholded = K.cast(y_pred > threshold, tf.float32)
 
         # axis depends on input shape
-        axis = tuple(range(1, len(y_pred.shape) - 1))
+        # axis = tuple(range(1, len(y_pred.shape) - 1))
+        axis = [1, 2]
 
         intersection = K.sum(K.abs(y_true * y_pred_thresholded), axis=axis)
         union = K.sum(y_true, axis=axis) + K.sum(y_pred_thresholded, axis=axis) - intersection
@@ -45,7 +46,8 @@ def get_soft_iou(smooth=0.001):
         y_true = K.cast(y_true, 'float32')
         y_pred = K.cast(y_pred, 'float32')
 
-        axis = tuple(range(1, len(y_pred.shape) - 1))
+        # axis = tuple(range(1, len(y_pred.shape) - 1))
+        axis = [1, 2]
 
         intersection = K.sum(K.abs(y_true * y_pred, axis=axis))
         union = K.sum(y_true, axis=axis) + K.sum(y_pred, axis=axis) - intersection
@@ -69,7 +71,8 @@ def get_dice_coeff(threshold=0.5, smooth=0.001):
         y_true = K.cast(y_true, 'float32')
         y_pred = K.cast(y_pred, 'float32')
 
-        axis = tuple(range(1, len(y_pred.shape) - 1))
+        # axis = tuple(range(1, len(y_pred.shape) - 1))
+        axis = [1, 2]
 
         y_pred_thresholded = K.cast(y_pred > threshold, tf.float32)
 
@@ -98,7 +101,8 @@ def get_soft_dice(epsilon=1e-6):
         y_pred = K.cast(y_pred, 'float32')
 
         # skip the batch and class axis for calculating Dice score
-        axis = tuple(range(1, len(y_pred.shape) - 1))
+        # axis = tuple(range(1, len(y_pred.shape) - 1))
+        axis = [1, 2]
 
         numerator = 2. * K.sum(y_pred * y_true, axis)
         denominator = K.sum(K.square(y_pred) + K.square(y_true), axis)
