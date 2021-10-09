ec439cf2f348ce395c0c1c49317dd827b756beab
soroush <soroush.moazed@gmail.com>
2021-10-09 08:55:18 +0330

    add test for dice

--- model/metric_test.py
+++ model/metric_test.py
@@ -36,7 +36,8 @@ def manual_iou_coef(y_true, y_pre, threshold=0.5):
     iou_score = np.sum(intersection) / np.sum(union)
     return iou_score
 
-def manual_dice_coef(y_true , y_pre, threshlod=0.5):
+
+def manual_dice_coef(y_true, y_pre, threshlod=0.5):
     y_pred = K.cast(y_pre > threshlod, 'float32')
     intersection = np.logical_and(y_true, y_pred)
     intersection2 = 2 * np.sum(intersection)
@@ -52,21 +53,18 @@ def test_iou_coef():
     iou_value = iou_metric(y_true, y_pred)
     iou_true = manual_iou_coef(y_true, y_pred)
 
-    # iou_coef = metric.iou_coef(y_true_1, y_pred_1)
     assert K.abs(iou_value - iou_true) < 0.001
 
-def test_dice_coef():
-    y_true = np.array([[1., 1., 1.],
-                     [1., 0., 1.],
-                     [1., 1., 0.]])
 
-    y_pred = np.array([[0.1, 0.1, 0.1],
-                     [0.8, 0.1, 0.8],
-                     [0.8, 0.8, 0.1]])
-    y_true_1, y_pred_1 = np.expand_dims(y_true, 0), np.expand_dims(y_pred, 0)
-    y_true_1, y_pred_1 = np.expand_dims(y_true_1, -1), np.expand_dims(y_pred_1, -1)
-    iou_coef1 = iou_coef(y_true_1,y_pred_1)
-    assert K.abs(float(iou_coef1) - manual_iou_coef(y_true , y_pred)) < 0.0001
+def test_dice_coef():
+    dice_metric = get_dice_coeff()
+
+    y_true, y_pred = example_tensor_data()
+
+    dice_value = dice_metric(y_true, y_pred)
+    dice_true = manual_dice_coef(y_true, y_pred)
+    assert K.abs(dice_true - dice_value) < 0.0001
+
 
 def test_hausdorff_distance():
     y_true, y_pred = example_tensor_data()
