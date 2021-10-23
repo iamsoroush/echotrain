b327af8c77adbb61ffe3fda77c11dc702281caf3
soroush <soroush.moazed@gmail.com>
2021-10-23 17:11:57 +0330

    syntax

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append('..')
 
+from model.base_class import BaseClass
 from .dataset_base import DatasetBase
 from .dataset_camus import CAMUSDataset
-from model.base_class import BaseClass
