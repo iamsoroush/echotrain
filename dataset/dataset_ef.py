a8fa75ceed43be602f9fb1278bc945043c044f06
zahra <zahraayenedast@gmail.com>
2021-11-07 19:23:22 +0330

    add . to imports add generate_y for labels type add doc string

--- dataset/dataset_ef.py
+++ dataset/dataset_ef.py
@@ -6,7 +6,21 @@ from model.pre_processing import PreProcessor
 
 class EfDataset:
     """
-    create dataset for ef
+    This class make dataset for EF models
+    Need a config file as input
+    The dataset can be based on image frames of ES and ED like :
+            x : images , y : volumes (y is volume of ES OR EV based on image state)
+    or dataset can be based on label(mask) frames of ES and ED like :
+            x : label(mask) , y : volumes (y is volume of ES OR EV based on image state)
+    x and y types are numpy.ndarray
+    and dataset can be created for subsets   {train . test , val }
+    Example :
+            EfDataset=EfDataset(config_file)
+            x_train , y_train = EfDataset.ef_dataset('image' , 'train')
+            x_val , y_val = EfDataset.ef_dataset('image' , 'val')
+            or
+            x_train , y_train = EfDataset.ef_dataset('label' , 'train')
+            x_val , y_val = EfDataset.ef_dataset('label' , 'val')
 
     """
 
@@ -27,9 +41,19 @@ class EfDataset:
             self.input_w = config.input_w
             self.n_channels = config.n_channels
             self.dataset_name = config.data_handler.target_dataset_name
-            self.dataset_class_path = config.dataset_class_path
+            self.dataset_class_path = config.dataset_class
 
     def load_dataframe(self, subset):
+        """
+        this method create dataframe based on subset and database
+        database type (camus or echonet) will be read from config file
+        input :
+        subset(string) : 'train' or 'test' or 'val'
+        return :
+        dataframe
+
+
+        """
 
         dataset_class = locate(self.dataset_class_path)
         dataset = dataset_class(self.config)
@@ -46,8 +70,15 @@ class EfDataset:
     def create_x_y(self, type, subset):
 
         """
+        this method create a list of image or label(mask) path and a list of EC , ED volumes
+        it uses dataframes for collecting path and volumes
+        input :
+        type(string) : 'image' or 'label'
+        subset(string) : 'train' or 'test' or 'val'
+        return :
+        list x(list) , y(list)
 
-        sebset should be one of trian , test , val
+
 
         """
 
@@ -78,14 +109,28 @@ class EfDataset:
         return x, y
 
     def prepare_x_y(self, x, y, type):
+        """
+        This method load images or labels(masks) from directory path
+        we use DatasetGenerator's generate_x for image type and generate_y for label type for loading data
+        we use PreProcessor class
+        at the end we convert x , y from list to numpy array
+        input :
+        x (list) list of images or labels(mask) path
+        y (list) list of volumes
+        type(string) : 'image' , 'label'
+        return :
+        x (np.ndarray) , y(np.ndarray)
+
+        """
         gen = DatasetGenerator(x, y, self.batch_size, (self.input_h, self.input_w), self.n_channels)
 
         preprocessor = PreProcessor(self.config)
 
-        x = gen.generate_x(x)
         if type == 'image':
+            x = gen.generate_x(x)
             x = np.array(list(map(preprocessor.img_preprocess, x)))
         else:
+            x = gen.generate_y(x)
             x = np.array(list(map(preprocessor.label_preprocess, x)))
 
         y = np.expand_dims(y, -1)
@@ -93,6 +138,20 @@ class EfDataset:
         return x, y
 
     def ef_dataset(self, type, subset):
+        """
+        this method created dataset
+        input :
+        type(string) : 'image' or 'label'
+        subset(string) : 'train' or 'test' or 'val'
+        return :
+        x (np.ndarray) , y(np.ndarray)
+
+        example :
+        x.shape : (2552, 256, 256, 1)
+        y.shape : (2552, 1)
+
+
+        """
 
         x, y = self.create_x_y(type, subset)
         x, y = self.prepare_x_y(x, y, type)
