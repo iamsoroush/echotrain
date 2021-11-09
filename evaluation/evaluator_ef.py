ef44674f328d0ba2fa7d5d477220ccc971975f5a
Sarmeili <sarmeili.f@gmail.com>
2021-11-09 09:39:49 +0330

    evaluator is added.v0.1

--- evaluation/evaluator_ef.py
+++ evaluation/evaluator_ef.py
@@ -2,6 +2,7 @@ from model.metric import mae, mse, r2_score
 from dataset.dataset_ef import EFDataset
 from pydoc import locate
 import pandas as pd
+import numpy as np
 
 class EFEvaluator:
 
@@ -19,14 +20,20 @@ class EFEvaluator:
                  'mae': [],
                  'mse': [],
                  'r2_score': []}
-        for i in range(len(ed_es_frames)):
-            df_ef['ef_pred'].append(self.model_class.ef_estimation(ed_es_frames[i][0], ed_es_frames[i][1]))
-            df_ef['ef_true'].append(ef_true[i])
-            df_ef['mae'].append(mae(ef_true[i], self.model_class.ef_estimation(ed_es_frames[i][0], ed_es_frames[i][1])))
-            df_ef['mse'].append(mse(ef_true[i], self.model_class.ef_estimation(ed_es_frames[i][0], ed_es_frames[i][1])))
-            df_ef['r2_score'].append(
-                r2_score(ef_true[i], self.model_class.ef_estimation(ed_es_frames[i][0], ed_es_frames[i][1])))
+        ef_pred = []
 
+        for ed_es_frame in ed_es_frames:
+            ef_pred.append(self.model_class.ef_estimation(ed_es_frame[0].reshape(1,112,112,1)
+                                                          ,ed_es_frame[1].reshape(1,112,112,1)))
+
+        ef_pred = np.array(ef_pred).reshape(-1,1)
+        ef_true = ef_true.reshape(-1,1)
+        for i in range(len(ef_true)):
+            df_ef['ef_true'].append(ef_true[i])
+            df_ef['ef_pred'].append(ef_pred[i])
+            df_ef['mae'].append(mae(ef_true[i],ef_pred[i]))
+            df_ef['mse'].append(mse(ef_true[i], ef_pred[i]))
+            df_ef['r2_score'].append(r2_score(ef_true[i], ef_pred[i]))
         return pd.DataFrame(df_ef)
 
     def _get_config(self):
