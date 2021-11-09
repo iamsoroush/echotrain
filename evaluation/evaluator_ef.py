0c6ba0a8d20a0a310ba43ca7475eae77d96d5d4e
Sarmeili <sarmeili.f@gmail.com>
2021-11-09 11:49:33 +0330

    evaluator is added.v1

--- evaluation/evaluator_ef.py
+++ evaluation/evaluator_ef.py
@@ -1,8 +1,11 @@
 from model.metric import mae, mse, r2_score
 from dataset.dataset_ef import EFDataset
+from dataset.dataset_echonet import EchoNetDataset
 from pydoc import locate
 import pandas as pd
 import numpy as np
+import os
+
 
 class EFEvaluator:
 
@@ -23,18 +26,33 @@ class EFEvaluator:
         ef_pred = []
 
         for ed_es_frame in ed_es_frames:
-            ef_pred.append(self.model_class.ef_estimation(ed_es_frame[0].reshape(1,112,112,1)
-                                                          ,ed_es_frame[1].reshape(1,112,112,1)))
+            ef_pred.append(self.model_class.ef_estimation(ed_es_frame[0].reshape(1, 112, 112, 1)
+                                                          , ed_es_frame[1].reshape(1, 112, 112, 1)))
 
-        ef_pred = np.array(ef_pred).reshape(-1,1)
-        ef_true = ef_true.reshape(-1,1)
+        ef_pred = np.array(ef_pred).reshape(-1, 1)
+        ef_true = ef_true.reshape(-1, 1)
         for i in range(len(ef_true)):
             df_ef['ef_true'].append(ef_true[i])
             df_ef['ef_pred'].append(ef_pred[i])
-            df_ef['mae'].append(mae(ef_true[i],ef_pred[i]))
+            df_ef['mae'].append(mae(ef_true[i], ef_pred[i]))
             df_ef['mse'].append(mse(ef_true[i], ef_pred[i]))
             df_ef['r2_score'].append(r2_score(ef_true[i], ef_pred[i]))
-        return pd.DataFrame(df_ef)
+
+        echonet = EchoNetDataset(self.config)
+        dataset_df = echonet.val_df_
+        dataset_df.reset_index(inplace=True)
+        dataset_df['ef_true'] = None
+        dataset_df['ef_pred'] = None
+        dataset_df['mae'] = None
+        dataset_df['mse'] = None
+        df_ef = pd.DataFrame(df_ef)
+        for i in range(len(dataset_df['case_id'].unique())):
+            dataset_df.loc[i * 2, ['ef_true', 'ef_pred', 'mae', 'mse']] = df_ef.loc[
+                i, ['ef_true', 'ef_pred', 'mae', 'mse']]
+            dataset_df.loc[i * 2 + 1, ['ef_true', 'ef_pred', 'mae', 'mse']] = df_ef.loc[
+                i, ['ef_true', 'ef_pred', 'mae', 'mse']]
+        dataset_df.to_csv(os.path.join(self.exported_dir, 'exported', 'dataframe_of_evaluation.csv'))
+        return dataset_df
 
     def _get_config(self):
 
@@ -43,7 +61,9 @@ class EFEvaluator:
             self.dataset_type = 'image'
             model_class_location = locate('model.ejection_fraction.ef_model_with_encoder.EFModel_Encoder')
             self.model_class = model_class_location(self.config)
+            self.exported_dir = self.config.exported_dir
         elif self.estimation_method == 'rp':
             self.dataset_type = 'label'
             model_class_location = locate('model.ejection_fraction.ef_model_with_rp.EFModel_RP')
             self.model_class = model_class_location(self.config)
+            self.exported_dir = self.config.exported_dir
