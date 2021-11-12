be0f80c318c06f590ff80e9b640faaca2000c8a2
benyaminghn <benjaminghahremani@gmail.com>
2021-11-12 17:26:26 +0330

    metric_test.py updated

--- model/metric_test.py
+++ model/metric_test.py
@@ -9,8 +9,8 @@ from scipy.spatial.distance import directed_hausdorff
 class TestClass:
 
     @pytest.fixture
-    def example_tensor_data(self):
-        """Returns 1*3*3 tensors"""
+    def tensor_data_test_case(self):
+        """Returns 1*3*3*1 tensors"""
 
         y_true = np.array([[1., 1., 1.], [1., 0., 1.], [0., 0., 0.]])
         y_pred = np.array([[0.6, 0.9, 0.7], [0.8, 0.1, 0.8], [0.1, 0.2, 0.3]])
@@ -48,56 +48,72 @@ class TestClass:
         dice_score = intersection2 / np.sum(y_pred + y_true)
         return dice_score
 
-    def test_iou_coef(self, example_tensor_data):
-        y_true, y_pred = example_tensor_data
+    @pytest.mark.parametrize("threshold, smooth", [
+        (0.5, 0.001)
+    ])
+    def test_iou_coef(self, tensor_data_test_case, threshold, smooth):
+        y_true, y_pred = tensor_data_test_case
 
-        iou_value = get_iou_coef()(y_true, y_pred)
+        iou_value = get_iou_coef(threshold, smooth)(y_true, y_pred)
         iou_true = self.manual_iou_coef(y_true, y_pred)
         print('iou_coef:', iou_value)
         assert iou_value.shape == ([1])
         assert 'float' in str(iou_value.dtype)
         assert K.abs(iou_value - iou_true) < 0.001
 
-    def test_dice_coef(self, example_tensor_data):
-        y_true, y_pred = example_tensor_data
+    @pytest.mark.parametrize("threshold, smooth", [
+        (0.5, 0.001)
+    ])
+    def test_dice_coef(self, tensor_data_test_case, threshold, smooth):
+        y_true, y_pred = tensor_data_test_case
 
-        dice_value = get_dice_coeff()(y_true, y_pred)
+        dice_value = get_dice_coeff(threshold, smooth)(y_true, y_pred)
         dice_true = self.manual_dice_coef(y_true, y_pred)
         print('dice_coef:', dice_value)
         assert dice_value.shape == ([1])
         assert 'float' in str(dice_value.dtype)
         assert K.abs(dice_true - dice_value) < 0.1
 
-    def test_hausdorff_distance(self, example_tensor_data):
-        y_true, y_pred = example_tensor_data
-        hd_result = get_hausdorff_distance(y_true.shape[1], y_true.shape[2])(y_true, y_pred)
+    @pytest.mark.parametrize("w, h", [
+        (3, 3)
+    ])
+    def test_hausdorff_distance(self, tensor_data_test_case, w, h):
+        y_true, y_pred = tensor_data_test_case
+        hd_result = get_hausdorff_distance(w, h)(y_true, y_pred)
         print('hausdorff_distance:', hd_result)
 
         assert hd_result.shape == ()
         assert 'float' in str(hd_result.dtype)
         assert K.abs(hd_result - self.manual_hd(y_true, y_pred)) < 0.001
 
-    def test_get_mad(self, example_tensor_data):
-        y_true, y_pred = example_tensor_data
-        mad = get_mad(3, 3)(y_true, y_pred)
+    @pytest.mark.parametrize("w, h", [
+        (3, 3)
+    ])
+    def test_get_mad(self, tensor_data_test_case, w, h):
+        y_true, y_pred = tensor_data_test_case
+        mad = get_mad(w, h)(y_true, y_pred)
         print('mad:', mad)
         assert mad.shape == ()
         assert 'float' in str(mad.dtype)
 
-    def test_get_soft_dice(self, example_tensor_data):
-        y_true, y_pred = example_tensor_data
+    @pytest.mark.parametrize("epsilon", [
+        1e-6
+    ])
+    def test_get_soft_dice(self, tensor_data_test_case, epsilon):
+        y_true, y_pred = tensor_data_test_case
 
-        soft_dice = get_soft_dice()(y_true, y_pred)
+        soft_dice = get_soft_dice(epsilon)(y_true, y_pred)
         print('soft_dice:', soft_dice)
         assert soft_dice.shape == ()
         assert 'float' in str(soft_dice.dtype)
 
-    def test_get_soft_iou(self, example_tensor_data):
-        y_true, y_pred = example_tensor_data
+    @pytest.mark.parametrize("smooth", [
+        0.001
+    ])
+    def test_get_soft_iou(self, tensor_data_test_case, smooth):
+        y_true, y_pred = tensor_data_test_case
 
-        soft_iou = get_soft_iou()(y_true, y_pred)
+        soft_iou = get_soft_iou(smooth)(y_true, y_pred)
         print('soft_dice:', soft_iou)
         assert soft_iou.shape == ([1])
         assert 'float' in str(soft_iou.dtype)
-
-
