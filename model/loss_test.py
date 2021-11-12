24cf7a2b24da3d875db8552a4f20ab81fad712b2
benyaminghn <benjaminghahremani@gmail.com>
2021-11-12 14:26:41 +0330

    loss_test.py updated

--- model/loss_test.py
+++ model/loss_test.py
@@ -11,39 +11,8 @@ from utils import load_config_file
 class TestClass:
 
     @pytest.fixture
-    def config(self):
-        config_path = "./config/config_example_echonet.yaml"
-        config = load_config_file(os.path.abspath(config_path))
-        return config
-
-    @pytest.fixture
-    def dataset(self, config):
-        dataset = EchoNetDataset(config)
-        return dataset
-
-    @pytest.fixture
-    def generator_inputs(self, dataset):
-        instance = {
-            'list_images_dir': dataset.x_train_dir,
-            'list_labels_dir': dataset.y_train_dir,
-            'batch_size': dataset.batch_size,
-            'input_size': dataset.input_size,
-            'n_channels': dataset.n_channels,
-            'channel_last': True,
-            'to_fit': dataset.to_fit,
-            'shuffle': dataset.shuffle,
-            'seed': dataset.seed
-        }
-        return instance
-
-    @pytest.fixture
-    def data_gen(self, generator_inputs):
-        data_gen = DatasetGenerator(**generator_inputs)
-        return data_gen
-
-    @pytest.fixture
     def tensor_data_test_case(self):
-        """Returns 3*3*1 tensors"""
+        """Returns 1*3*3 tensors"""
 
         y_true = np.array([[1., 1., 1.], [1., 0., 1.], [0., 0., 0.]])
         y_pred = np.array([[0.6, 0.9, 0.7], [0.8, 0.1, 0.8], [0.1, 0.2, 0.3]])
@@ -87,6 +56,7 @@ class TestClass:
         y_true, y_pred = tensor_data_test_case
         loss = soft_iou_loss(y_true, y_pred)
         print('soft_iou_loss:', loss)
+
         assert loss <= 1
         assert loss.shape == ()
         assert 'tensor' in str(type(loss))
