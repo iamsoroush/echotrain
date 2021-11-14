e2fc60cebd3bfd411d1d9220edcaffc2803ecc5d
zahra <zahraayenedast@gmail.com>
2021-11-14 19:04:04 +0330

    changes for using datagenerator in hpo_baseline.py

--- hpo/hpo_baseline.py
+++ hpo/hpo_baseline.py
@@ -1,18 +1,20 @@
 import keras_tuner as kt
-from hypermodel_unet_baseline import HyperModel
+from .hypermodel_unet_baseline import HyperModel
 
 
 class HPOBaseline:
 
     def __init__(self, config):
         self.config = config
+        self._get_parameters()
 
     def _get_parameters(self):
-        self.objective = kt.Objective("iou_coef", direction="max"),
+        self.objective = kt.Objective("iou_coef", direction="max")
         self.max_trials = 3
         self.overwrite = True
         self.directory = ''
         self.project_name = "tune_hypermodel"
+        self.epoch_tuner = 2
 
     def generate_tuner(self):
         tuner = kt.RandomSearch(
@@ -26,9 +28,14 @@ class HPOBaseline:
 
         return tuner
 
-    def search_hp(self, x, y):
+    def search_hp(self, train_generator, validation_generator, n_iter_train, n_iter_val):
         tuner = self.generate_tuner()
-        tuner.search(x, y, epochs=self.epoch_tuner)
+        # tuner.search(x, y, epochs=self.epoch_tuner)
+        tuner.search(train_generator,
+                     steps_per_epoch=n_iter_train,
+                     epochs=self.epoch_tuner,
+                     validation_data=validation_generator,
+                     validation_steps=n_iter_val, )
         return tuner
 
     @staticmethod
@@ -39,10 +46,12 @@ class HPOBaseline:
     def get_tuner_summary(tuner):
         return tuner.result_summary()
 
-    def retrain_model(self, x, y):
+    def retrain_model(self, train_generator, validation_generator, n_iter_train, n_iter_val):
         hypermodel = HyperModel(self.config)
-        tuner = self.search_hp(x, y)
+        tuner = self.search_hp(train_generator, validation_generator, n_iter_train, n_iter_val)
         best_hp = self.get_best_hp(tuner)
 
         model = hypermodel.build(best_hp)
-        hypermodel.fit(best_hp, model, x, y, epochs=1)
+        # hypermodel.fit(best_hp, model, train_generator, validation_generator, epochs=1)
+        hypermodel.fit(best_hp, model, train_generator, steps_per_epoch=n_iter_train,
+                       validation_data=validation_generator, validation_steps=n_iter_val, epochs=1)
