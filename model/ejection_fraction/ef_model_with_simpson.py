4b38db75f49da62a7513f56c5f1995373a8dd265
Kimia_ghodoosi <kimiaghodoosi@gmail.com>
2022-01-18 09:00:14 +0330

    ef_model_with_simpson.py is fixed

--- model/ejection_fraction/ef_model_with_simpson.py
+++ model/ejection_fraction/ef_model_with_simpson.py
@@ -9,9 +9,7 @@ import skimage.io as io
 import pickle
 import numpy as np
 import math
-import matplotlib.pyplot as plt
-# % matplotlib
-# inline
+
 
 class EFModel_Simpson(EFBase):
 
@@ -32,27 +30,11 @@ class EFModel_Simpson(EFBase):
         """
         pi = math.pi
         interval = 5
-        ef_pred = []
-        volumes = []
         label_frame = frame.astype(np.int64).reshape(112, 112)
+        volume = 0
         for i in range(0, len(label_frame), interval):
-            volume = (label_frame[i].sum() / 2) ** 2 * pi
+            volume += (label_frame[i].sum() / 2) ** 2 * pi
         return volume
-        # for patient in ed_es:
-        #     areas = []
-        #     for ed_es_frames in patient:
-        #         area = 0
-        #         for i in range(0, len(ed_es_frames), interval):
-        #             area += (ed_es_frames[i].sum() / 2) ** 2 * pi
-        #         areas.append(area)
-            # ef_pred.append((areas[0] - areas[1]) / areas[0])
-        # rp = [regionprops(label_frame)[0].area,
-        #       regionprops(label_frame)[0].major_axis_length,
-        #       ]
-        # area = rp[0]
-        # length = rp[1]
-        # vol = 0.85 * (area * area) / length
-        # return vol
 
     def ef_estimation(self, ed_frame, es_frame):
         ed_vol = self.simpson_volume(ed_frame)
@@ -81,9 +63,6 @@ class EFModel_Simpson(EFBase):
             self.estimation_method = self.config.estimation_method
             if self.estimation_method == "simpson":
                 self.al_address = self.config.al.al_address
-                # self.al_to_vol_model_dir = self.config.al.al_to_vol_model_dir
-                # self.al_to_vol_model_type = self.config.al.train.model_type
-                # self.exported_dir = self.config.exported_dir
             else:
                 raise ValueError
         except ValueError:
